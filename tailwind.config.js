9a68cb76de1d111f902aeee41ec768e920c7ecbd
Yosmar Hinestroza <yosmarpc@gmail.com>
2024-11-08 08:25:10 -0300

    repositorio

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+    './node_modules/preline/preline.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('preline/plugin')
+  ],
+}
+
